3252d7d55bd9d5cef4146272d6a9c6bb12568b3a
RichardWooSJTU <RichardWooSJTU@users.noreply.github.com>
2020-02-17 16:02:41 +0800

    更新方法2，但是内存消耗没有明显改善

--- removeNthFromEnd/removeNthFromEnd/main.cpp
+++ removeNthFromEnd/removeNthFromEnd/main.cpp
@@ -3,7 +3,9 @@
 #include "ListNode.h"
 using namespace std;
 
-ListNode* removeNthFromEnd(ListNode* head, int n) {
+ListNode* removeNthFromEnd(ListNode* head, int n) 
+{
+    /*
     vector<ListNode*> pointerArr;
     int cnt = 0;
     
@@ -30,20 +32,42 @@ ListNode* removeNthFromEnd(ListNode* head, int n) {
     int location = cnt - n;
     pointerArr[location - 1]->next = pointerArr[location + 1];
     return pointerArr[0];
+    */
+    /////////////////////////////////////////////////////////
+    /////////使用双指针first 和 second 减小内存占用//////////
+    /////////////////////////////////////////////////////////
+    ListNode* first = head; ListNode* second = head;
+    int cnt = 0;
+    while (first != NULL)
+    {                 
+        first = first->next;
+        
+        if (cnt > n)
+            second = second->next;
+        ++cnt;
+    }
+    if (cnt == 1)
+        return NULL;
+    else if (n == cnt)
+    {
+        return second->next;
+    }
+    second->next = second->next->next;
+    return head;
 }
 
 int main(int argc, char** argv)
 {
     ListNode* head = new ListNode(1);
     ListNode* pNode = head;
-    /*
+    
     for (int i = 1; i != 5; ++i)
     {
         pNode->next = new ListNode(i + 1);
         pNode = pNode->next;
 
-    }*/
-    ListNode* result = removeNthFromEnd(head, 1);
+    }
+    ListNode* result = removeNthFromEnd(head, 2);
     while (result != NULL)
     {
         cout << result->val << endl;
